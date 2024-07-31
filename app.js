f3af63c5a0d72e0b3939fbc0fab1a9f4844fd117
Joel Assagou <liamstiduotechnology@gmail.com>
2024-07-31 19:30:25 +0000

    f comt

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
   });
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
